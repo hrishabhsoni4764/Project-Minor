71e3ce556d6fcd2cd59f8b235dd9a9b4c46b80f8
howlmortar <juliandealdrey@gmail.com>
2016-11-29 18:14:01 +0100

    Key+Door

---
+++ Assets/Key.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+    public GameObject doorToOpen;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.GetComponent<ThirdPersonController>()) {
+            doorToOpen.GetComponent<RoomDoorScript>().active = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/RoomDoorScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomDoorScript : MonoBehaviour {
+
+    [HideInInspector] public bool active = false;
+
+	void Update () {
+        if (active) {
+            GetComponent<BoxCollider>().isTrigger = true;
+        }
+	}
+}
